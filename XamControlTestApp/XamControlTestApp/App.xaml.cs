058133c613b531270f1e7881a07440bba4530b67
Wes Marco <wmarco@themarcos.net>
2019-04-18 10:56:28 -0400

    First checkin

---
+++ XamControlTestApp/XamControlTestApp/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamControlTestApp.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XamControlTestApp
+{
+    public partial class App : Application
+    {
+        private static App thisApp;
+        public static bool IsUserLoggedIn { get; set; }
+        public App()
+        {
+            thisApp = this;
+            IsUserLoggedIn = false;
+            InitializeComponent();           
+            SwitchMainPage();
+        }
+
+
+        public static App CurrentApp
+        {
+            get
+            {
+                return thisApp;
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        public void SwitchMainPage()
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                if (!IsUserLoggedIn)
+                {
+                    //Show Login PAge
+                    MainPage = new LoginPage();
+                }
+                else
+                {
+                    MainPage = new MainPage();
+                }
+            });
+        }
+
+    }
+}
---
+++ XamControlTestApp/XamControlTestApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamControlTestApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+        private string password = "password";
+        private string user = "user@me.com";
+		public LoginPage ()
+		{
+			InitializeComponent ();
+            slResetPW.IsVisible = false;
+
+            eEmail.Text = user;
+            ePassword.Text = password;
+
+            btnLogin.Clicked            += BtnLogin_Clicked;
+            btnForgotPassword.Clicked   += BtnForgotPassword_Clicked;
+            btnResetSave.Clicked        += BtnResetSave_Clicked;
+            btnResetCancel.Clicked      += BtnResetCancel_Clicked;
+		}
+
+        private void BtnResetCancel_Clicked(object sender, EventArgs e)
+        {
+            slLogin.IsVisible = true;
+            slResetPW.IsVisible = false;
+        }
+
+        private void BtnResetSave_Clicked(object sender, EventArgs e)
+        {
+            password = eNewPassword.Text;
+        }
+
+        private void BtnForgotPassword_Clicked(object sender, EventArgs e)
+        {
+            slLogin.IsVisible = false;
+            slResetPW.IsVisible = true;
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            if (eEmail.Text == user && ePassword.Text == password)
+            {
+                App.IsUserLoggedIn = true;
+                App.CurrentApp.SwitchMainPage();
+            }
+        }
+
+        private void ENewPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            eNewPasswordConfirm.Text = eNewPassword.Text;
+        }
+    }
+}
